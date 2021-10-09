0b835ef71aad4e996105c5e6da5f7222e8c189c8
Alexey <AlekseiTiutenkov@mail.ru>
2021-10-09 12:47:08 +0700

    Added a selector component and its functionality

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from "moment";
+import {DateService} from "../shared/date.service";
+
+interface Day {
+  value: moment.Moment;
+  active: boolean;
+  disabled: boolean;
+  selected: boolean;
+}
+
+interface Week {
+ days: Day[];
+}
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +20,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  calendar: Week[] = [];
+
+  constructor(
+    public dateService: DateService
+  ) { }
 
   ngOnInit(): void {
+    this.dateService.date.subscribe(this.generate.bind(this))
+  }
+
+  generate(now: moment.Moment) {
+    console.log(now.format())
   }
 
 }
