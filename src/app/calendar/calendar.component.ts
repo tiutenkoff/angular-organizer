fb383c8868437ba8e4ac3051f9344e9044a04606
Alexey <AlekseiTiutenkov@mail.ru>
2021-10-09 13:32:58 +0700

    Added a calendar component and its functionality

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -27,11 +27,39 @@ export class CalendarComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.dateService.date.subscribe(this.generate.bind(this))
+    this.dateService.date.subscribe(this.generate.bind(this)); 
   }
 
   generate(now: moment.Moment) {
-    console.log(now.format())
+    const startDay = now.clone().startOf('month').startOf('week');
+    const endDay = now.clone().endOf('month').endOf('week');
+
+    const date = startDay.clone().subtract(1, 'day');
+
+    const calendar = [];
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone();
+            const active = moment().isSame(value, 'date');
+            const disabled = !now.isSame(value, 'month');
+            const selected = now.isSame(value, 'date');
+            return {
+              value,
+              active,
+              disabled,
+              selected
+            };
+        })
+      })
+    }
+    this.calendar = calendar;
+  }
+
+  select(day: moment.Moment) {
+    this.dateService.changeDate(day);
   }
 
 }
